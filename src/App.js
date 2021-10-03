c6127ffa77669666f6f6de580cf41ebb79481c47
Buddhika <buddhikadananjaya55@gmail.com>
2021-10-03 23:59:42 +0530

    add some packages and changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ParticlesBg from "particles-bg";
+import MouseParticles from 'react-mouse-particles';
 
-function App() {
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            covidData: null,
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://hpb.health.gov.lk/api/get-current-statistical")
+            .then((Response) => Response.json())
+            .then((users) => {
+                console.log(users);
+                this.setState({
+                    covidData: users.data,
+                });
+            });
+    }
+
+    render() {
   return (
     <div className="App">
       <header className="App-header">
