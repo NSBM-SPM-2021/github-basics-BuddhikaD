f6dc27ad77d0ddeb71fd4cdd2357efdee781d1f5
Buddhika <buddhikadananjaya55@gmail.com>
2021-10-06 23:12:24 +0530

    Updates

---
+++ precache-manifest.40717d114d720b052c848deb7cdff483.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ddde36a90e8fbeb15ee7a50a703c58",
+    "url": "/github-basics-BuddhikaD/index.html"
+  },
+  {
+    "revision": "8f12662859852954c642",
+    "url": "/github-basics-BuddhikaD/static/css/main.a25c5c15.chunk.css"
+  },
+  {
+    "revision": "0ae825b6aa5227ef8805",
+    "url": "/github-basics-BuddhikaD/static/js/2.747c8b0d.chunk.js"
+  },
+  {
+    "revision": "cbe01fbbe035bf90d35a835621f136ef",
+    "url": "/github-basics-BuddhikaD/static/js/2.747c8b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f12662859852954c642",
+    "url": "/github-basics-BuddhikaD/static/js/main.9090fe35.chunk.js"
+  },
+  {
+    "revision": "dbf617f80650c33ed4ab",
+    "url": "/github-basics-BuddhikaD/static/js/runtime-main.662d7bba.js"
+  }
+]);
